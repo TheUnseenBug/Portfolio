3801a8f6e933e58bc95429a50fc06c44d648051d
TheUnseenBug <dennis.granheimer@live.se>
2022-05-24 17:05:57 +0200

    1

--- src/Component/Experience.js
+++ src/Component/Experience.js
@@ -1,21 +1,10 @@
-import { Grid, Paper, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { styled } from "@mui/material/styles";
 import React from "react";
+import { Grid, Paper, Typography, Box } from "@mui/material";
 import Node from "./Images/nodejs-new-white.png";
 import reactLogo from "./Images/react-2.png";
 import jsLogo from "./Images/logo-javascript.png";
-import htmlLogo from "./Images/html-1.png";
 
 const Experience = () => {
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
-
   return (
     <Box sx={{ marginTop: "100px", maxWidth: "100%" }}>
       <Typography
@@ -58,19 +47,37 @@ const Experience = () => {
         </Typography>
       </Box>
       <Grid container direction="row" justifyContent="center" spacing={4}>
-        <Grid item xs={6} md={4}>
-          <Paper elevation={10} sx={{ width: "100%" }}>
-            <img src={reactLogo} style={{ width: "100%" }} />
+        <Grid item xs={6} md={3}>
+          <Paper
+            elevation={10}
+            sx={{ width: "100%", paddingTop: "100%", position: "relative" }}
+          >
+            <img
+              src={reactLogo}
+              style={{ width: "100%", position: "absolute", top: 0, left: 0 }}
+            />
           </Paper>
         </Grid>
-        <Grid item xs={6} md={4}>
-          <Paper elevation={10} sx={{ width: "100%", paddingTop: "100%" }}>
-            <img src={Node} style={{ width: "100%" }} />
+        <Grid item xs={6} md={3}>
+          <Paper
+            elevation={10}
+            sx={{ width: "100%", paddingTop: "100%", position: "relative" }}
+          >
+            <img
+              src={Node}
+              style={{ width: "100%", position: "absolute", top: 0, left: 0 }}
+            />
           </Paper>
         </Grid>
-        <Grid item xs={6} md={4}>
-          <Paper elevation={10} sx={{ width: "100%" }}>
-            <img src={jsLogo} style={{ width: "100%" }} />
+        <Grid item xs={6} md={3}>
+          <Paper
+            elevation={10}
+            sx={{ width: "100%", paddingTop: "100%", position: "relative" }}
+          >
+            <img
+              src={jsLogo}
+              style={{ width: "100%", position: "absolute", top: 0, left: 0 }}
+            />
           </Paper>
         </Grid>
       </Grid>
@@ -82,7 +89,7 @@ const Experience = () => {
           gap: { lg: "6%", xs: 5 },
         }}
       >
-        {/* <Typography variant="h4" py={3} sx={{ color: "#40495fad" }}>
+        <Typography variant="h4" py={3} sx={{ color: "#40495fad" }}>
           Photoshop
         </Typography>
         <Typography variant="h4" sx={{ color: "#40495fad" }}>
@@ -90,7 +97,7 @@ const Experience = () => {
         </Typography>
         <Typography variant="h4" sx={{ color: "#40495fad" }}>
           Illustrator
-        </Typography> */}
+        </Typography>
       </Box>
     </Box>
   );

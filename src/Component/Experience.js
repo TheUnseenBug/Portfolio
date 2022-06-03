d57a541e1c173a173085583938580fc3ae2174dd
TheUnseenBug <dennis.granheimer@live.se>
2022-06-03 21:45:40 +0200

    Some fixing around design

--- src/Component/Experience.js
+++ src/Component/Experience.js
@@ -42,8 +42,8 @@ const Experience = () => {
         variant="h3"
         sx={{
           display: "flex",
-          justifyContent: "center",
           alignItems: "center",
+          justifyContent: "center",
           color: "text.primary",
         }}
       >
